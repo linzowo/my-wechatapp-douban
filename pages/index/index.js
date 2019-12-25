db000f72d270021c20ffa9aabbf80bf454b70cae
linzowo <linzowo@outlook.com>
2019-12-25 22:56:13 +0800

    功能开发 #003 完成主页列表渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,40 @@
-Page({})
+const app = getApp();
+
+Page({
+  data: {
+    list: [
+      { key: "in_theaters" },
+      { key: "coming_soon" },
+      { key: "new_movies" },
+      { key: "top250" }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+    wx.showLoading({ title: "拼命加载中..." });
+
+    //   生成任务列表
+    const tasks = this.data.list.map(item => {
+      return app.douban.find(item.key, 1, 8).then(d => {
+        item.title = d.title;
+        item.movies = d.subjects;
+        return item;
+      });
+    });
+
+    //   完成任务列表
+    Promise.all(tasks).then(list => {
+      this.setData({
+        list: list
+      });
+
+      console.log(this.data.list);
+      
+      
+      wx.hideLoading();
+    });
+  }
+});
