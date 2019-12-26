c565d80d28374dfea591953aa31728c474043b2f
linzowo <linzowo@outlook.com>
2019-12-26 15:31:20 +0800

    功能开发 #006 完成list列表渲染功能开发

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,1 +1,59 @@
-Page({})
+// 电影榜单页面功能
+
+const app = getApp();
+
+Page({
+  data: {
+    title: "",
+    type: "in_theaters",
+    list: [],
+    page: 1,
+    size: 20,
+    hasMore: true
+  },
+
+  /**
+   * 加载更多
+   */
+  loadMore() {
+    wx.showLoading({ title: "拼命加载中..." });
+    // 根据id查找对应的电影详情
+    return app.douban
+      .find(this.data.type, this.data.page++, this.data.size)
+      .then(d => {
+        this.setData({
+          title: d.title,
+          list: this.data.list.concat(d.subjects),
+          hasMore: this.data.page - 1 < Math.ceil(d.total / this.data.size)
+        });
+        console.log(this.data);
+
+        wx.hideLoading();
+      })
+      .catch(err => {
+        this.setData({ title: "获取数据异常" });
+        console.error(err);
+        wx.hideLoading();
+      });
+  },
+  /**
+   * 页面加载时调用的方法
+   */
+  onLoad(params) {
+    this.setData({ type: params.type });
+    this.loadMore();
+  },
+
+  /**
+   * 用户下来响应
+   */
+  onPullDownRefresh() {
+    this.setData({ list: [], page: 1, hasMore: true });
+    this.loadMore().then(() => wx.stopPullDownRefresh);
+  },
+
+  onReachBottom() {
+    if (!this.data.hasMore) return;
+    this.loadMore();
+  }
+});
